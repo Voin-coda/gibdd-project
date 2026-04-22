d131447ec3bd153ed3502ae538bc7c70ab126102
Voin-coda <gleb.yakin.05@mail.ru>
2026-04-22 20:37:34 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -74,7 +74,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
